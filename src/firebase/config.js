d21b40a6b87caac756ee780da9ea2bb6da1c46c6
ginokalyun <ginokalyun@gmail.com>
2020-08-03 13:03:47 +0200

    Configured for Firebase storage

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCJOoIoomVz2adfiiWAeJyYXQZePcx3CUU",
+  authDomain: "ginogram-f7916.firebaseapp.com",
+  databaseURL: "https://ginogram-f7916.firebaseio.com",
+  projectId: "ginogram-f7916",
+  storageBucket: "ginogram-f7916.appspot.com",
+  messagingSenderId: "398222626725",
+  appId: "1:398222626725:web:5e3c6f529193671e6ea24c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export {projectStorage, projectFirestore};
